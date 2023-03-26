deff81357c4eef03a1ff2448784a9c828f23b083
Alexander Ramos <126944755+aramos1977@users.noreply.github.com>
2023-03-26 10:28:06 -0500

    Taller1Javascript

---
+++ taller1.js
@@ -0,0 +1,13 @@
+
+/* Definir dos variables, asignarles un valor y utilizando 
+    decisiones imprimir en la consola la que tenga menor valor.
+    Número menor */
+var a =prompt("Ingrese el primer número");
+            var b =prompt("Ingrese el segundo número");
+            if(a===b)
+                alert("Son Iguales");
+            if(a<b)
+                alert("el número menor es : "+a);
+            if(b<a)
+                alert("el número menor es : "+b);
+                
